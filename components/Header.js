6c5922290e805977397499d584412550ab1a524c
Daniel Brott <danbrott42@gmail.com>
2023-03-11 07:13:46 +0000

    hooked up tailwind and setup initial page layout

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  <div>
+    <footer>
+
+    </footer>
+  </div>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'regal-blue': '#243c5a',
+        'lab-green': '#24cca9',
+        'other-green': '#ecfdf5',
+        'form-green': '#6fe6b7',
+        'button-green': '#15b981',
+        'form-input-box-green': '#a8f4d0',
+        'table-cell-darker-green': '#16a34a',
+        'table-cell-lighter-green': '#86efac',
+        'table-cell-green': '#35d399',
+        'th-green': '#15b981',
+        'fake-button': '#d1fae4',
+      },
+   },
+  },
+  plugins: [],
+}
